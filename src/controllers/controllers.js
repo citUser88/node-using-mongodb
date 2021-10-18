eaf4a7e320bd36d268cfc7b2beaf0feada8bf06a
hiLowLevel <8507390+citUser88@users.noreply.github.com>
2021-10-17 20:14:21 -0700

    update controller.js (space)

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -6,7 +6,7 @@ const Product = mongoose.model('Product', ProductSchema);
 export const addnewProduct = (req, res) => {
   let newProduct = new Product(req.body);
 
-  newProduct.save((err,Product) => {
+  newProduct.save((err, Product) => {
     if (err) {
       res.send(err);
     }
